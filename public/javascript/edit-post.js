f9cc66b2e0e551e5708964ace8c491cfe0c749dc
Jonathan Johnson <jonathandila682@gmail.com>
2022-04-16 21:57:43 -0500

    refactor code

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,24 +1,20 @@
 async function editFormHandler(event) {
     event.preventDefault();
-
-    const title = document.querySelector('input[name="post-title"]').value.trim();
-    const content = document.querySelector('input[name="content"]').value.trim();
-    console.log(title);
-    console.log(content);
-
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_content = document.querySelector('input[name="post-content"]').value;
     const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-      
-      const response = await fetch(`/api/posts/${id}`, {
+        window.location.toString().split('/').length - 1
+      ];
+
+    const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
-          post_id: id,
-          title,
-          content
+            title,
+            post_content
         }),
         headers: {
-          'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
         }
       });
       
@@ -27,7 +23,6 @@ async function editFormHandler(event) {
       } else {
         alert(response.statusText);
       }
-
-}
-
-document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+  }
+  
+  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
